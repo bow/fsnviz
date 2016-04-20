b8f652034b3ea44ab29ec6e707e161ff6078c0d0
bow <bow@bow.web.id>
2016-04-20 10:00:05 +0200

    Add coverage check to .travis.yml

--- fsnviz/tests/test_general.py
+++ fsnviz/tests/test_general.py
@@ -9,12 +9,24 @@
     :license: BSD
 
 """
+from os import path
+
 from click.testing import CliRunner
 
 from fsnviz.main import cli
+from .utils import get_test_path
 
 
 def test_help():
     runner = CliRunner()
     cmd = runner.invoke(cli, ["--help"])
     assert cmd.exit_code == 0
+
+
+def test_star_fusion_ok():
+    runner = CliRunner()
+    in_path = get_test_path("star_fusion_v060_01.txt")
+    with runner.isolated_filesystem():
+        cmd = runner.invoke(cli, ["star-fusion", in_path])
+        assert cmd.exit_code == 0
+        assert path.exists("fsnviz.svg")
