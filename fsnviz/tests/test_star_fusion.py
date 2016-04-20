998da00047d0039aab260b2afc16eee611a99614
bow <bow@bow.web.id>
2016-04-20 11:33:13 +0200

    Add star-fusion tests

--- fsnviz/tests/test_star_fusion.py
+++ fsnviz/tests/test_star_fusion.py
@@ -17,10 +17,64 @@ from fsnviz.main import cli
 from .utils import get_test_path
 
 
+FNAME1 = get_test_path("star_fusion_v060_01.txt")
+
+
 def test_star_fusion_ok():
     runner = CliRunner()
-    in_path = get_test_path("star_fusion_v060_01.txt")
     with runner.isolated_filesystem():
-        cmd = runner.invoke(cli, ["star-fusion", in_path])
+        cmd = runner.invoke(cli, ["star-fusion", FNAME1])
         assert cmd.exit_code == 0
         assert path.exists("fsnviz.svg")
+
+
+def test_star_fusion_circos_exe_fail():
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        params = ["--circos-exe", "/nonexistent/file"]
+        cmd = runner.invoke(cli, params + ["star-fusion", FNAME1])
+        assert cmd.exit_code != 0
+        assert "Could not find an executable circos at '/nonexistent/file'" \
+            in cmd.output
+        assert not path.exists("fsnviz.svg")
+
+
+def test_star_fusion_out_dir_ok():
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        out_dir = "my_dir"
+        params = ["--out-dir", out_dir]
+        cmd = runner.invoke(cli, params + ["star-fusion", FNAME1])
+        assert cmd.exit_code == 0
+        assert path.exists(path.join(out_dir, "fsnviz.svg"))
+        assert not path.exists("fsnviz.svg")
+
+
+def test_star_fusion_base_name_ok():
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        base_name = "my_stuff"
+        params = ["--base-name", base_name]
+        cmd = runner.invoke(cli, params + ["star-fusion", FNAME1])
+        assert cmd.exit_code == 0
+        assert path.exists("{0}.svg".format(base_name))
+        assert not path.exists("fsnviz.svg")
+
+
+def test_star_fusion_karyotype_ok():
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        params = ["--karyotype", "human.hg19"]
+        cmd = runner.invoke(cli, params + ["star-fusion", FNAME1])
+        assert cmd.exit_code == 0
+        assert path.exists("fsnviz.svg")
+
+
+def test_star_fusion_karyotype_png_ok():
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        params = ["--png"]
+        cmd = runner.invoke(cli, params + ["star-fusion", FNAME1])
+        assert cmd.exit_code == 0
+        assert path.exists("fsnviz.svg")
+        assert path.exists("fsnviz.png")
