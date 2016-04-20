a8b4c006852308adbcf0111c0724b6f61cb3d53c
bow <bow@bow.web.id>
2016-04-20 10:39:36 +0200

    Add test for default star-fusion run

---
+++ fsnviz/tests/test_star_fusion.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+    fsnviz.tests.test_star_fusion
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    STAR-Fusion subcommand tests.
+
+    :copyright: (c) 2016 Wibowo Arindrarto <bow@bow.web.id>
+    :license: BSD
+
+"""
+from os import path
+
+from click.testing import CliRunner
+
+from fsnviz.main import cli
+from .utils import get_test_path
+
+
+def test_star_fusion_ok():
+    runner = CliRunner()
+    in_path = get_test_path("star_fusion_v060_01.txt")
+    with runner.isolated_filesystem():
+        cmd = runner.invoke(cli, ["star-fusion", in_path])
+        assert cmd.exit_code == 0
+        assert path.exists("fsnviz.svg")
---
+++ fsnviz/tests/utils.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+    fsnviz.tests.utils
+    ~~~~~~~~~~~~~~~~~~
+
+    General test utilities.
+
+    :copyright: (c) 2016 Wibowo Arindrarto <bow@bow.web.id>
+    :license: BSD
+
+"""
+from os.path import abspath, dirname, join
+
+
+TEST_CASE_DIR = abspath(join(dirname(__file__), "cases"))
+
+
+def get_test_path(bname, test_dir=TEST_CASE_DIR):
+    """Helper method to return the path of a test case file or directory.
+
+    :param bname: Test case base name.
+    :type bname: str
+    :param test_dir: Test case directory name.
+    :type test_dir: str
+    :returns: Absolute path to the test case file or directory.
+    :rtype: str
+
+    """
+    return abspath(join(test_dir, bname))
