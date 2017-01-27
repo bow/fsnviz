95217a2a2e6d58a1e457d69733c5232ccf0c618e
bow <bow@bow.web.id>
2017-01-27 15:59:29 +0100

    Rename history to changelog

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,7 +1,7 @@
 .. :changelog:
 
-History
-=======
+Changelog
+=========
 
 
 Versions 0.2
--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ from fsnviz import __author__, __contact__, \
 with open("README.rst") as src:
     readme = src.read()
 
-with open("HISTORY.rst") as src:
-    history = src.read().replace(".. :changelog:", "").strip()
+with open("CHANGELOG.rst") as src:
+    changelog = src.read().replace(".. :changelog:", "").strip()
 
 with open("requirements.txt") as src:
     requirements = [line.strip() for line in src]
@@ -25,7 +25,7 @@ setup(
     version=__version__,
     description="Tool for plotting gene fusion events detected by"
                 " various tools using Circos.",
-    long_description=readme + "\n\n" + history,
+    long_description=readme + "\n\n" + changelog,
     author=__author__,
     author_email=__contact__,
     url=__homepage__,
